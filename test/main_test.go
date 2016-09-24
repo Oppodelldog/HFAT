55fada26c261941f2e8e3d0f90655fa3964e5546
odog <odog@netcologne.de>
2016-09-24 12:28:44 +0200

    passthrough status code from primary to client

--- test/main_test.go
+++ test/main_test.go
@@ -3,11 +3,11 @@ package test
 import (
 	"HFAT/server"
 	"bytes"
+	"io/ioutil"
 	"net/http"
 	"reflect"
 	"strconv"
 	"testing"
-	"io/ioutil"
 )
 
 const TEST_SERVER_1_PORT = 10000
@@ -17,20 +17,22 @@ const TEST_SERVER_2_RESPONSE_BODY = "TEST SERVER 2 RESPONSE"
 const LOCALHOST = "localhost"
 const HFAT_SERVER_PORT = 8000
 const TEST_PATH_AND_QUERY = "/whateverPath?TEST=98&sha=27892572805702572"
+const TEST_SERVER_1_STATUS_CODE = 200
+const TEST_SERVER_2_STATUS_CODE = 299
 
 func TestTimeConsuming(t *testing.T) {
 
 	requestSnifferChannel1 := make(chan *http.Request)
 	requestSnifferChannel2 := make(chan *http.Request)
-	go StartTestHttpServer(TEST_SERVER_1_PORT, TEST_SERVER_1_RESPONSE_BODY, requestSnifferChannel1)
-	go StartTestHttpServer(TEST_SERVER_2_PORT, TEST_SERVER_2_RESPONSE_BODY, requestSnifferChannel2)
+	go StartTestHttpServer(TEST_SERVER_1_PORT, TEST_SERVER_1_RESPONSE_BODY, TEST_SERVER_1_STATUS_CODE, requestSnifferChannel1)
+	go StartTestHttpServer(TEST_SERVER_2_PORT, TEST_SERVER_2_RESPONSE_BODY, TEST_SERVER_2_STATUS_CODE, requestSnifferChannel2)
 
 	go server.StartHFATServer(HFAT_SERVER_PORT, []server.ForwardingTarget{
 		{Server: LOCALHOST, Port: TEST_SERVER_1_PORT},
 		{Server: LOCALHOST, Port: TEST_SERVER_2_PORT, Primary: true},
 	})
 
-	testRequest, err := http.NewRequest("GET", "http://" + LOCALHOST + ":" + strconv.Itoa(HFAT_SERVER_PORT) + TEST_PATH_AND_QUERY, bytes.NewBufferString(""))
+	testRequest, err := http.NewRequest("GET", "http://"+LOCALHOST+":"+strconv.Itoa(HFAT_SERVER_PORT)+TEST_PATH_AND_QUERY, bytes.NewBufferString(""))
 	if err != nil {
 		t.Error(err)
 	}
@@ -57,8 +59,8 @@ func TestTimeConsuming(t *testing.T) {
 		t.Error("raw query sniffed at server2 is not equal to test request raw query")
 	}
 
-	if response.StatusCode != TEST_SERVER_RESPONSE_STATUS_CODE {
-		t.Error("Did expect status code %v, but not: %v\n", TEST_SERVER_RESPONSE_STATUS_CODE, response.StatusCode)
+	if response.StatusCode != TEST_SERVER_2_STATUS_CODE {
+		t.Error("Did expect status code %v, but not: %v\n", TEST_SERVER_2_STATUS_CODE, response.StatusCode)
 	}
 
 	defer response.Body.Close()
--- test/test_server.go
+++ test/test_server.go
@@ -6,14 +6,13 @@ import (
 	"strconv"
 )
 
-const TEST_SERVER_RESPONSE_STATUS_CODE = 200
-
 // Fires up a test http-server on localhost and the given port.
 // Via the given channel it will return any request he receives.
-func StartTestHttpServer(port int, responseString string, requestChannel chan *http.Request) {
+func StartTestHttpServer(port int, responseString string, statusCode int, requestChannel chan *http.Request) {
 	testServer := TestServer{
 		responseString: responseString,
 		requestChannel: requestChannel,
+		statusCode:     statusCode,
 	}
 	http.ListenAndServe(":"+strconv.Itoa(port), testServer)
 }
@@ -21,11 +20,12 @@ func StartTestHttpServer(port int, responseString string, requestChannel chan *h
 type TestServer struct {
 	requestChannel chan *http.Request
 	responseString string
+	statusCode     int
 }
 
 func (ts TestServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ts.requestChannel <- r
-	w.WriteHeader(TEST_SERVER_RESPONSE_STATUS_CODE)
+	w.WriteHeader(ts.statusCode)
 	w.Write(bytes.NewBufferString(ts.responseString).Bytes())
 
 }
