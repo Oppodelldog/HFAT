deddcf61507432f11e93cbac24ce7d283c443dd8
odog <odog@netcologne.de>
2016-10-20 23:29:15 +0200

    removed interactive mode

--- ci/run-docker-build.sh
+++ ci/run-docker-build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -it -v $(pwd):/go/src/HFAT golang:1.7.3 /bin/bash /go/src/HFAT/ci/build.sh
+docker run -v $(pwd):/go/src/HFAT golang:1.7.3 /bin/bash /go/src/HFAT/ci/build.sh
