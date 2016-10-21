dc35a678b029cf2da1b8ad1408be9c21ac78c5e2
odog <odog@netcologne.de>
2016-10-21 23:31:16 +0200

    only test HFAT (not go tools etc)

--- ci/docker-container/test-cobertura.sh
+++ ci/docker-container/test-cobertura.sh
@@ -2,4 +2,4 @@
 echo "getting tools for code cover report conversion (for cobertura plugin)"
 go get github.com/axw/gocov/...
 go get github.com/AlekSi/gocov-xml
-gocov test ./... | gocov-xml > /go/src/HFAT/coverage.xml
+gocov test HFAT/... | gocov-xml > /go/src/HFAT/coverage.xml
--- ci/docker-container/test-junit.sh
+++ ci/docker-container/test-junit.sh
@@ -4,4 +4,4 @@ echo "getting tool for test report conversion (for junit plugin)"
 go get -u github.com/jstemmer/go-junit-report
 
 echo "run tests"
-go test -v ./... | go-junit-report > /go/src/HFAT/report.xml
+go test -v HFAT/... | go-junit-report > /go/src/HFAT/report.xml
