7f77c12a3354609818f5ad47b14de90b21b17209
odog <odog@netcologne.de>
2016-10-21 23:52:09 +0200

    added compier warnings

--- ci/docker-container/compiler-warnings.sh
+++ ci/docker-container/compiler-warnings.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
+echo "lint & vet for compiler warnings"
 go get -u github.com/golang/lint/golint
 
-# Run lint tools (Compiler warning plugin)
-golint HFAT/... > lint.txt
-
-# Run vet tools (Compiler warning plugin)
-go vet HFAT/... > vet.txt
+golint HFAT/... >  /go/src/HFAT/lint.txt
+go vet HFAT/... >  /go/src/HFAT/vet.txt
